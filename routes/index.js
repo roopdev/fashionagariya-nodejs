424ef66dc247feea7ca2bf9b75ffdf622a2a2449
roopdev <as.roopesh@gmail.com>
2016-11-22 14:22:14 +0530

    fashionnagariya

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('shop/index', { title: 'FashionNagariya' });
 });
 
+router.get('/about', function(req, res, next) {
+	res.render('shop/about');
+});
+
 router.get('/single', function(req, res, next) {
   res.render('shop/single', { title: 'FashionNagariya' });
 });
 
 router.get('/products', function(req, res, next) {
   res.render('shop/products', { title: 'FashionNagariya' });
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+  res.render('shop/shopping-cart', { title: 'FashionNagariya' });
+});
+
+router.get('/checkout', function(req, res, next) {
+  res.render('shop/checkout', { title: 'FashionNagariya' });
 });
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,103 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
 
-/* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.render('user/login');
+var Order = require('../models/order');
+var Cart = require('../models/cart');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+router.get('/profile', isLoggedIn, function(req, res, next) {
+	Order.find({user: req.user}, function(err, orders) {
+		if (err) {
+			return res.write('No order list Yet! ');
+		}
+		var cart;
+		orders.forEach(function(order) {
+			cart = new Cart(order.cart);
+			order.items = cart.generateArray();
+		});
+		res.render('user/profile', {orders: orders });
+	});
+});
+
+router.get('/logout', isLoggedIn, function(req, res, next) {
+	req.logout();
+	res.redirect('/');
+});
+
+router.use('/', notLoggedIn, function(req, res, next) {
+	next();
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('user/register');
+	var messages = req.flash('error');
+	res.render('user/register', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
+});
+
+router.post('/register', passport.authenticate('local.signup', {
+	failureRedirect: '/user/register',
+	failureFlash: true
+}), function (req, res, next) {
+	if (req.session.oldUrl) {
+		var oldUrl = req.session.oldUrl;
+		req.session.oldUrl = null;
+		res.redirect(oldUrl);
+	} else {
+		res.redirect('/user/profile');
+		}
+	});
+
+router.get('/resend', function(req, res, next) {
+	res.render('user/resend');
+});
+
+router.post('/resend', function(req, res, next) {
+	// Fill in the function to resend verification mail
+});
+
+router.get('/login', function(req, res, next) {
+	var messages = req.flash('error');
+	res.render('user/login', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
+});
+
+router.post('/login', passport.authenticate('local.signin', {
+	failureRedirect: '/user/login',
+	failureFlash: true
+}), function (req, res, next) {
+	if (req.session.oldUrl) {
+		var oldUrl = req.session.oldUrl;
+		req.session.oldUrl = null;
+		res.redirect(oldUrl);
+	} else {
+		res.redirect('/user/profile');
+		}
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('user/contact', { title: 'FashionNagariya' });
+	res.render('user/contact');
 });
+
+router.post('/contact', function(req, res, next) {
+	// Add the function to send the input contact 
+	// Response should be an success message on same page
+	// Respond with thank you mail to the recipient. 
+});
+
 module.exports = router;
+
+function isLoggedIn (req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/');
+}
+
+function notLoggedIn (req, res, next) {
+	if (!req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/');
+}
