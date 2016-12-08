f305e53155861c741523a789443c039d5e3701b3
roopdev <as.roopesh@gmail.com>
2016-12-08 16:11:15 +0530

    fashionagariya

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Cart = require('../models/cart');
 var Product = require('../models/product');
+var Order = require('../models/order');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -35,8 +37,27 @@ router.get('/products', function(req, res, next) {
 	});
 });
 
+router.get('/add-to-cart/:id', function(req, res, next) {
+	var productId = req.params.id;
+	var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+	Product.findById(productId, function(err, product) {
+		if (err) {
+			return res.redirect('/');
+		}
+		cart.add(product, product.id);
+		req.session.cart = cart;
+		console.log(req.session.cart);
+		res.redirect('/products');
+	});
+});
+
 router.get('/shopping-cart', function(req, res, next) {
-  res.render('shop/shopping-cart', { title: 'FashionNagariya' });
+	if (!req.session.cart) {
+		return res.render('shop/shopping-cart', {products: null});
+	}
+	var cart = new Cart(req.session.cart);
+	res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 router.get('/checkout', function(req, res, next) {
